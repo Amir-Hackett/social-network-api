30a12861e973f5ef9e82196647fb4b0008aaac35
Amir <amir.hackett@gmail.com>
2022-06-24 09:11:16 -0400

    fixed errors

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -6,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(require('./routes/index'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
-    useFindAndModify: false,
+    // MongoDB will find and connect to the database if it exists or create the database if it doesn't
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
